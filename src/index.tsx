99d9f066af552c61e43b17b049f6edcda2d36277
navdeyev <navdeyev@gmail.com>
2018-04-15 16:28:54 +0100

    Introduced redux store and basic routing.

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,26 @@
+import {History} from 'history';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {browserHistory, Route, Router} from 'react-router'
+import {syncHistoryWithStore} from 'react-router-redux'
+import {Store} from 'redux';
+
+import initStore from 'domains/initStore';
+import {IAppState} from 'domains/types';
+
 import App from './App';
 import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+
+const store: Store<IAppState> = initStore();
+const history: History = syncHistoryWithStore(browserHistory, store);
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
+    <Provider store={store}>
+        <Router history={history}>
+            <Route path="/" component={App}/>
+        </Router>
+    </Provider>,
+    document.getElementById('root') as HTMLElement
 );
-registerServiceWorker();
+
