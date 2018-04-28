838daa3fe4399438a13390dea62721f532162dd0
navdeyev <navdeyev@gmail.com>
2018-04-28 12:01:07 +0100

    Header needs to be static on the page, scroll moved to PageContentHolder.

--- src/components/PageLayout/PageLayout.tsx
+++ src/components/PageLayout/PageLayout.tsx
@@ -6,6 +6,7 @@ import {Heading1} from 'components/Styled/Styled';
 import {rem} from 'components/Styled/utils';
 
 const Page = styled.div`
+    width: 100%;
     display: flex;
     flex-direction: column;
 `;
@@ -14,8 +15,8 @@ Page.displayName = 'Page';
 const PageHeader = styled.header`
     display: flex;
     align-items: center;
-    height: ${ rem(60) };
     padding: ${ rem(20) };
+    flex: 0 0 ${ rem(60) };
     background-color: ${ colors.DARK_GREY };
 `;
 PageHeader.displayName = 'PageHeader';
@@ -23,7 +24,11 @@ PageHeader.displayName = 'PageHeader';
 const PageTitle = Heading1.extend`color: ${ colors.WHITE };`;
 PageTitle.displayName = 'PageTitle';
 
-const PageContentHolder = styled.div`padding-top: ${ rem(16) };`;
+const PageContentHolder = styled.div`
+    flex: 1 0 0px;
+    overflow-y: scroll;
+    padding-top: ${ rem(16) };
+`;
 PageContentHolder.displayName = 'PageContentHolder';
 
 export interface IProps {
