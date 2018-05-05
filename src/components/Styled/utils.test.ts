441015c0c1b8d3f93cafd010de34123d4d7c72db
navdeyev <navdeyev@gmail.com>
2018-05-05 09:29:26 +0100

    Added support for media types and different font sizes on mobile-tablet-desktop window sizes.

--- src/components/Styled/utils.test.ts
+++ src/components/Styled/utils.test.ts
@@ -1,9 +1,30 @@
-import {rem} from './utils';
+import {media, rem} from './utils';
 
 describe('rem', () => {
     it('converts the passed down px value to rems', () => {
         expect(rem(0)).toBe('0rem');
         expect(rem(16)).toBe('1rem');
         expect(rem(24)).toBe('1.5rem');
+    });
+});
+
+describe('media', () => {
+    it('wraps the passed css with a mobile media type', () => {
+        const wrapped = media.mobile`font-size: 12px;`;
+        expect(wrapped.toString()).toBe(
+            '@media ,only screen and (max-width: 600px), { ,font-size: 12px;, }'
+        );
+    });
+    it('wraps the passed css with a tablet media type', () => {
+        const wrapped = media.tablet`font-size: 16px;`;
+        expect(`${ wrapped }`).toBe(
+            '@media ,only screen and (min-width: 601px) and (max-width: 1023px), { ,font-size: 16px;, }'
+        );
+    });
+    it('wraps the passed css with a desktop media type', () => {
+        const wrapped = media.desktop`font-size: 20px;`;
+        expect(`${ wrapped }`).toBe(
+            '@media ,only screen and (min-width: 1024px), { ,font-size: 20px;, }'
+        );
     });
 });
--- src/components/Styled/utils.ts
+++ src/components/Styled/utils.ts
@@ -1,4 +1,21 @@
+import {css, SimpleInterpolation} from 'styled-components';
+
 export const rem = (px: number): string => {
     return `${px / 16}rem`;
 };
 
+enum MediaTypes {
+    MOBILE = 'only screen and (max-width: 600px)',
+    TABLET = 'only screen and (min-width: 601px) and (max-width: 1023px)',
+    DESKTOP = 'only screen and (min-width: 1024px)'
+}
+
+const createMediaWrapper = (mediaType: MediaTypes) =>
+    (strings: TemplateStringsArray, ...interpolations: SimpleInterpolation[]) =>
+        css`@media ${mediaType} { ${ css(strings, ...interpolations) } }`;
+
+export const media = {
+    mobile: createMediaWrapper(MediaTypes.MOBILE),
+    tablet: createMediaWrapper(MediaTypes.TABLET),
+    desktop: createMediaWrapper(MediaTypes.DESKTOP)
+};
